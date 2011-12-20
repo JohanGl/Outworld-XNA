59f128e7948f281d6768076ed325ff2c0d2864bc
q3maniac <q3maniac@hotmail.com>
2011-12-20 21:49:53 +0100

    ...

---
+++ System/Game.World/Terrains/Helpers/TileCollisionHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Game.World.Terrains.Parts.Areas;
+using Game.World.Terrains.Parts.Tiles;
+using Microsoft.Xna.Framework;
+
+namespace Game.World.Terrains.Helpers
+{
+	public class TileCollisionHelper
+	{
+		public Tile GetIntersectingTile(Area area, Vector3 point)
+		{
+			// If the current point is inside the area
+			if (area.Info.BoundingBox.Contains(point) == ContainmentType.Contains)
+			{
+				// Calculate the point indices within the area tile array
+				int x = Math.Min((Area.Size.X - 1), (int)(point.X - area.Info.BoundingBox.Min.X));
+				int y = Math.Min((Area.Size.Y - 1), (int)(area.Info.BoundingBox.Max.Y - point.Y));
+				int z = Math.Min((Area.Size.Z - 1), (int)(point.Z - area.Info.BoundingBox.Min.Z));
+
+				// Get the tile
+				return area.Info.Tiles[(Area.LevelSize * y) + (z * Area.Size.X) + x];
+			}
+
+			var resultTile = new Tile();
+			resultTile.Type = TileType.Empty;
+			
+			return resultTile;
+		}
+	}
+}
