b2a52fe84ab12caf78e51f64e856bead068b7c31
q3maniac <q3maniac@hotmail.com>
2012-01-10 21:24:14 +0100

    ...

---
+++ System/Game.Package.Outworld/Scenes/InGame/Controls/Hud/RadarEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Outworld.Scenes.InGame.Controls.Hud
+{
+	public class RadarEntity
+	{
+		public int Id;
+		public Vector3 Position;
+		public RadarEntityColor Color;
+
+		public enum RadarEntityColor
+		{
+			Yellow
+		};
+
+		public void Initialize(int id, RadarEntityColor color, Vector3 position)
+		{
+			Id = id;
+			Color = color;
+			Position = position;
+		}
+	}
+}
