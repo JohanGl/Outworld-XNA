36f7bda0798bebfd267a5ec83052b265767cce54
Johan Glysing <johan.glysing@gmail.com>
2012-01-17 21:28:21 +0100

    ...

--- System/Game.Package.Outworld/GamePackage.cs
+++ System/Game.Package.Outworld/GamePackage.cs
@@ -59,12 +59,11 @@ namespace Outworld
 			gameContext.Resources.Textures.Add("Global.TerrainMergeMask", content.Load<Texture2D>(@"Terrain\TerrainMergeMask"));
 
 			// Initialize the root scene of this game package
-			//gameContext.Scenes.Add(new MainMenuScene());
-			//gameContext.Scenes.Add(new NewGameScene());
+			gameContext.Scenes.Add(new MainMenuScene());
 			//gameContext.Scenes.Add(new TerrainDebugScene());
 			//gameContext.Scenes.Add(new ModelScene());
 			//gameContext.Scenes.Add(new AudioScene());
-			gameContext.Scenes.Add(new NetworkScene());
+			//gameContext.Scenes.Add(new NetworkScene());
 		}
 
 		public void Shutdown()
